eca82fd6fe95e63a93d9378b241a8c4aa6fc07e3
valadao02 <136474600+valadao02@users.noreply.github.com>
2023-07-17 14:56:09 -0300

    Add files via upload

---
+++ src/componentes/Menu/Icones/index.jsx
@@ -0,0 +1,9 @@
+export default function Icone({ icone, style }) {
+    return (
+      <li className={style.menu__item}>
+        <img src={icone.path} alt={icone.alt} />
+        <a href="/">{icone.link}</a>
+      </li>
+    )
+  }
+  
